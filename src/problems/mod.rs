6ad51e60fa91da550bd772294b033ded1b232ce3
Joe Ellis <ellis125@gmail.com>
2025-10-20 17:46:08 +1300

    Problem 34

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -31,3 +31,4 @@ mod problem_030;
 mod problem_031;
 mod problem_032;
 mod problem_033;
+mod problem_034;
---
+++ src/problems/problem_034.rs
@@ -0,0 +1,17 @@
+use crate::utils::numbers::Digits;
+
+#[test]
+fn problem_034() {
+    let result: i64 = (10..3_000_000)
+        .filter(|&x| digit_factorial_sum(x) == x)
+        .sum();
+    println!("{}", result);
+}
+
+fn digit_factorial_sum(num: i64) -> i64 {
+    num.digits().into_iter().map(factorial).sum()
+}
+
+fn factorial(num: i64) -> i64 {
+    (1..=num).product()
+}
